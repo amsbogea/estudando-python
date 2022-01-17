5ab198efb05d3dc7c65baae6722b8baa80372839
Alex Miranda <32579231+amsbogea@users.noreply.github.com>
2022-01-17 03:02:25 -0300

    Aula 2 na Dio

---
+++ app_python/aula2.py
@@ -0,0 +1,20 @@
+a = int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+#print('soma: {}'.format(soma))
+
+print('soma: {soma}. '
+      '\nsubtracao: {subtracao}. '
+      '\nmultiplicacao: {multiplicacao}. '
+      '\ndivisao: {divisao} '
+      '\nresto: {resto}'.format(soma=soma, subtracao=subtracao, multiplicacao=multiplicacao, divisao=divisao, resto=resto))
+
+# print ('soma: ' + str(soma))
+# print ('subtracao: ' + str(subtracao))
+# print ('multiplicacao: ' + str(multiplicacao))
+# print (divisao)
+# print (resto)
